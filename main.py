d163befeee8e00ab551328c1f05eddf63e2a6409
Dara.Adekore <oluwadara.adekore@student.ufv.ca>
2021-04-26 15:41:13 -0700

    Script is now able to: *Take in user input & Look for the Song *Ask for the user specify which artist they would like lyrics from *Pull up the lyrics from the artist and store them in a text file

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
+import operator
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 # A python program to search up a song and give you options based on which artists have a song by the given name
 # upon choosing which song version you would like, the song lyrics are displayed
 
 PATH = "C:/Users/adeko/Downloads/chromedriver_win32/chromedriver"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://genius.com/")
+driver.get("https://www.lyrics.com/")
 
-main = driver.find_element_by_name("q")
-print("What song would you like to search..")
-print("\n")
-main.send_keys(input())
+main = driver.find_element_by_name("st")
+
+main.send_keys(input("What song would you like to search.."))
 
 main.send_keys(Keys.RETURN)
 
 driver.get(driver.current_url)  # need to update the current page document that we're working with.
-main = driver.find_element_by_xpath('/html/body/routable-page/ng-outlet/search-results-page/div/div[2]/div[1]/div['
-                                    '2]/search-result-section/div')
-cards = main.find_elements_by_class_name('mini_card-info')
-for elemnts in cards:
-    print("---------------------------------")
-    print(elemnts.text)
+# main = driver.find_element_by_css_selector('Button.btn.btn-3.btn-xs.primary')  not being used at the moment.
+# ^ just used to learn how to use css selector
+main = driver.find_element_by_id('content-body')
+result_list = main.find_elements_by_css_selector('p.lyric-meta-album-artist')
+for results in result_list:
+    print(results.text)
 
-selection = input("which version would you like?")
-for elemnts in cards:
-
-    if selection in elemnts.text:
-        elemnts.click()
+selection = input("Which artist would you like...")
+result_list2 = main.find_elements_by_xpath('//div[@class="sec-lyric clearfix"]')
+for results in result_list2:
+    if operator.contains(results.text, selection):
+        results.click()
         break
 
 driver.get(driver.current_url)  # need to update the current page document that we're working with.
-main = driver.find_element_by_xpath('/html/body/routable-page/ng-outlet/song-page/div/div/div[2]/div['
-                                    '1]/div/defer-compile[1]/lyrics/div/div/section/p')
-print(main.text)
+main = driver.find_element_by_class_name("lyric.clearfix")
+lyrics = main.find_element_by_xpath('//pre[@id="lyric-body-text"]')
+title = driver.title + ".txt"
+f = open(title, "a")
+f.write(lyrics.text)
+f.close()
